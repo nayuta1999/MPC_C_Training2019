a4a7b207c6fb73c7113673c957d4cfb98d21b9b8
sakaki_tohru <48438137+sakaki-tohru@users.noreply.github.com>
2019-05-14 15:09:43 +0900

    問１模範解答追加
    疑問:これ、新1年生が講義前に見れてしまうのでは?

---
+++ code/02_Answer/sakaki_exe01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+	int a,b;
+	scanf("%d%d",&a,&b);
+
+	if(a<b){
+		printf("<\n");
+	}
+	else if(a==b){
+		printf("=\n");
+	}
+	else{
+		printf(">\n");
+	}
+
+	return 0;
+}
