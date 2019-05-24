3bec278a2588a71d13a8ae76338473de53280cb5
Santaro Hanafusa <undertale1104@gmail.com>
2019-05-24 23:37:10 +0900

    make 05_exercise

---
+++ 05_Answer/code/papyrus/exercise1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void){
+  int player_hp, player_atk;
+  int enemy_hp, enemy_atk;
+
+  printf("player_hp = ");
+  scanf("%d", &player_hp);
+  printf("player_atk = ");
+  scanf("%d", &player_atk);
+  printf("enemy_hp = ");
+  scanf("%d", &enemy_hp);
+  printf("enemy_atk = ");
+  scanf("%d", &enemy_atk);
+
+  while(1){
+    enemy_hp -= player_atk;
+    if(enemy_hp <= 0){
+      printf("Win\n");
+      break;
+    }
+
+    player_hp -= enemy_atk;
+    if(player_hp <= 0){
+      printf("Lose\n");
+      break;
+    }
+  }
+}
