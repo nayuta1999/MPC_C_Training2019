ae28c8b00b935b48d99ada6f5bf2253cfedf2bf6
maltpuff <50700843+maltpuff@users.noreply.github.com>
2019-06-26 14:55:14 +0900

    Update func1.c
    コメント文の追加

--- 06/code/func1.c
+++ 06/code/func1.c
@@ -9,10 +9,11 @@ int main(void) {
 	int num, i;
 	double x;
 
-	//
+	//データを渡すことのできる関数の例
 	srand(334);
 
-	printf("randam\n");
+	
+	printf("乱数ゾーン/n");
 	for (i = 0; i < 5; i++) {
 		num = rand();
 		printf("%d\n", num);
@@ -20,12 +21,12 @@ int main(void) {
 
 	printf("%d\n", rand());//このようにprintf()の中に書くこともできる
 
-	//
-	printf("ランダムゾーン math.h");
+	//データを渡してその結果を受け取ることができる関数の例
+	printf("math.hの関数を使ってみよう");
 	x = cos(PI / 3);
 	printf("%lf\n", x);
 
-	x = pow(2, 4);
+	x = pow(2, 4);　//このように2つ引数を与えるものもあります
 	printf("%lf\n", x);
 
 	abort();
