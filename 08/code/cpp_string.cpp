5abfcc87c8daa483b3a9591df0bb719288355431
nayuta1999 <youmu331@gmail.com>
2019-07-09 23:20:52 +0900

    C++とpythonのソースファイルを追加

---
+++ 08/code/cpp_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+	string str="Yeah!";
+	cout<<str.length()<<endl;
+	str+="aaa!!!";
+	cout<<str<<endl;
+}
---
+++ 08/code/python_string.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+str="Year!"
+print(len(str));
+str=str+"aaa!!!"
+print(str);
+
